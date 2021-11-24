cbbf7eaa7f8a938778a0f06a8af396e46cea754c
VaneChira <vanessachira26@gmail.com>
2021-11-24 20:02:51 +0200

    get latest changes from master

--- src/main/java/com/bootcamp/demo/service/CardService.java
+++ src/main/java/com/bootcamp/demo/service/CardService.java
@@ -34,20 +34,4 @@ public class CardService {
         updateDetails.put("expirationMonth", cardDetails.getExpirationMonth());
         this.firestoreDao.update(id,"cards", updateDetails);
     }
-
-    public void addCard(Card card){
-        firestoreDao.addCard(card);
-    }
-
-    public Card setPreferredCard(String cardNumber) {
-        return firestoreDao.setPreferredCard(cardNumber);
-    }
-
-    public void addCard(Card card){
-        firestoreDao.addCard(card);
-    }
-
-    public Card setPreferredCard(String cardNumber) {
-        return firestoreDao.setPreferredCard(cardNumber);
-    }
 }
