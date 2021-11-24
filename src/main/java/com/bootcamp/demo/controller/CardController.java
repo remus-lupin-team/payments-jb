0058f2bf113af072302dc2bb353a41da47f2ef9e
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-11-24 16:49:03 +0200

    Updated the card request body to use card, and used the information to create a map for the dao update method

--- src/main/java/com/bootcamp/demo/controller/CardController.java
+++ src/main/java/com/bootcamp/demo/controller/CardController.java
@@ -1,11 +1,13 @@
 package com.bootcamp.demo.controller;
 
+import com.bootcamp.demo.model.Card;
 import com.bootcamp.demo.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import java.util.List;
@@ -23,9 +25,9 @@ public class CardController {
     }
 
     @DeleteMapping(path="{id}")
-    ResponseEntity<String> removeCard(@PathVariable String id){
-        this.cardService.removeCard(id);
-        return new ResponseEntity<>("Successfully deleted card "+ id, HttpStatus.OK);
+   public ResponseEntity<String> removeCard(@PathVariable String id){
+      this.cardService.removeCard(id);
+      return new ResponseEntity<>("Successfully deleted card "+ id, HttpStatus.OK);
     }
 
     @GetMapping("/getAllCards")
@@ -33,9 +35,15 @@ public class CardController {
          return cardService.getAll();
     }
 
-    @PutMapping(path="{id}", consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<String> updateCard(@PathVariable String id, @RequestBody Map cardDetails){
-        this.cardService.updateCard(id, cardDetails);
+  public @PutMapping(path="{id}", consumes = APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateCard(@PathVariable String id, @RequestBody Card cardDetails){
+        Map updateDetails = new HashMap();
+        updateDetails.put("cardNumber", cardDetails.getCardNumber());
+        updateDetails.put("holderName", cardDetails.getHolderName());
+        updateDetails.put("cvv", cardDetails.getCVV());
+        updateDetails.put("expirationYear", cardDetails.getExpirationYear());
+        updateDetails.put("expirationMonth", cardDetails.getExpirationMonth());
+        this.cardService.updateCard(id, updateDetails);
         return new ResponseEntity<>("Successfully updated card "+ id, HttpStatus.OK);
     }
 }
