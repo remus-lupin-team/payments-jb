f2795bfc5bf57d082b3eca50c16becff3bdaf021
VaneChira <vanessachira26@gmail.com>
2021-11-24 21:27:28 +0200

    cardStateEnum code clean

--- src/main/java/com/bootcamp/demo/model/CardStateEnum.java
+++ src/main/java/com/bootcamp/demo/model/CardStateEnum.java
@@ -1,11 +1,6 @@
 package com.bootcamp.demo.model;
 
 public enum CardStateEnum {
-    NONE(0),
-    PREFERRED(1);
-
-    int state;
-    CardStateEnum(Integer state){
-        this.state = state;
-    }
+    NONE,
+    PREFERRED;
 }
