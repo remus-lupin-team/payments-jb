d1f416fbb5941f93938956b75e60608dd53910f6
Mihai <mitzagrebla@yahoo.com>
2021-11-25 12:06:32 +0200

    Added TransactionController for tracking

---
+++ src/main/java/com/bootcamp/demo/Controller/TransactionController.java
@@ -0,0 +1,28 @@
+package com.bootcamp.demo.controller;
+
+import com.bootcamp.demo.model.Transaction;
+import com.bootcamp.demo.service.TransactionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/history", produces = APPLICATION_JSON_VALUE)
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/all")
+    public List<Transaction> getAll(){
+        System.out.println(transactionService.getAll());
+        return transactionService.getAll();
+    }
+
+}
